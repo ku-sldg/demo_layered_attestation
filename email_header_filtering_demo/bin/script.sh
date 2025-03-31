2487298c11505dc14fa711bd913481f82dc96e8c
Will Thomas <52861844+Durbatuluk1701@users.noreply.github.com>
2025-03-31 11:54:49 -0500

    Update script.sh

--- email_header_filtering_demo/bin/script.sh
+++ email_header_filtering_demo/bin/script.sh
@@ -4,16 +4,7 @@
 
 # NOTE: Before running this, export DEMO_ROOT = ...your path here.../LayeredAttestation/src/demo_layered_attestation
 
-
 : "${DEMO_ROOT:?DEMO_ROOT must be set to run this script}"
-
-
-### This file includes a locally correct value for DEMO_ROOT.
-### If you would like to use this script on your system,
-### please edit this line to the correct value in your
-### filesystem
-
-export DEMO_ROOT=/Users/guttman/scm/LayeredAttestation/src/demo_layered_attestation # Your value here!  
 
 export DEMO_SRC=$DEMO_ROOT/email_header_filtering_demo 
 export CDS_INSTALLED=$DEMO_ROOT/installed_dir
